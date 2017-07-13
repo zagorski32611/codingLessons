541a1d47e7f3b4e574d8e692646f812c7ff42a8e
zagorski32611 <j.j.zagorski@gmail.com>
2017-07-13 07:38:01 -0400

    update 7-13-17

--- Codecademy/Ruby/blocksLambda.rb
+++ Codecademy/Ruby/blocksLambda.rb
@@ -34,3 +34,47 @@ Recall that the ! in Ruby means "this method could do something dangerous or
 unexpected!" In this case, it mutates the original array instead of creating
 a new one.
 =end
+
+
+def yield_name(name)
+  puts "In the method! Let's yield."
+  yield("Kim")
+  puts "In between the yields!"
+  yield(name)
+  puts "Block complete! Back in the method."
+end
+
+yield_name("Eric") { |n| puts "My name is #{n}." }
+
+
+def horsepwer(num)
+  puts "How many does it make?"
+  yield("320")
+  puts "It makes 320"
+  yield(num)
+  puts num * 2
+end
+horsepower(320)
+
+
+#This is one the works
+def double(num)
+  yield(num)
+end
+
+double(16) {|x| puts x * 2}
+
+floats = [1.2, 3.45, 0.91, 7.727, 11.42, 482.911]
+# Write your code below this line!
+round_dowm = Proc.new do |n|
+  n.round_down
+end
+
+
+# Write your code above this line!
+ints = floats.collect(&round_down)
+print ints
+
+over_4_feet = Proc.new do |height|
+  return height >= 4
+end
