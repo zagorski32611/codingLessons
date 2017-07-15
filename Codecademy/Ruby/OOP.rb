ac5ff3496752990022a678feef3a5d5d42abf59b
zagorski32611 <j.j.zagorski@gmail.com>
2017-07-14 23:30:57 -0400

    OOP 1 complete

--- Codecademy/Ruby/OOP.rb
+++ Codecademy/Ruby/OOP.rb
@@ -234,3 +234,309 @@ end
 
 err = SuperBadError.new
 err.display_error
+
+=begin
+In Ruby, inheritance works like this:
+
+class DerivedClass < BaseClass
+  # Some stuff!
+end
+The derived class is the new class you're making and the base class is the class from which that new class inherits. You can read "<" as "inherits from."
+Instructions
+1.
+We've created an Application class in the editor to the right. Create your own class, MyApp, that inherits from Application.
+No need to put anything inside your class definition just yet!
+=end
+
+class Application
+  def initialize(name)
+    @name = name
+  end
+end
+
+# Add your code below!
+class MyApp < Application
+end
+
+=begin
+Sometimes you'll want one class that inherits from another to not only take on the methods and attributes of its parent, but to override one or more of them.
+
+For instance, you might have an Email class that inherits from Message. Both classes might have a send method that sends them, but the e-mail version may have to
+ identify valid e-mail addresses and use a bunch of e-mail protocols that Message knows nothing about. Rather than add a send_email method to your derived class and
+ inherit a send method you'll never use, you can instead just explicitly create a send method in the Email class and have it do all the email-sending work.
+
+This new version of send will override (that is, replace) the inherited version for any object that is an instance of Email.
+Instructions
+1.
+Let's try a more entertaining (if less realistic) example. Create a new class, Dragon, that inherits from Creature. Give your derived class a fight method that
+overrides Creature's; instead of returning "Punch to the chops!", it should return "Breathes fire!".
+
+=end
+class Creature
+  def initialize(name)
+    @name = name
+  end
+
+  def fight
+    return "Punch to the chops!"
+  end
+end
+
+# Add your code below!
+class Dragon < Creature
+  def initialize(name)
+    @name = name
+  end
+
+  def fight
+    return "Breathes fire!"
+  end
+end
+
+
+=begin
+On the flip side, sometimes you'll be working with a derived class (or subclass) and realize that you've overwritten a method or
+attribute defined in that class' base class (also called a parent or superclass) that you actually need. Have no fear!
+You can directly access the attributes or methods of a superclass with Ruby's built-in super keyword.
+
+The syntax looks like this:
+
+class DerivedClass < Base
+  def some_method
+    super(optional args)
+      # Some stuff
+    end
+  end
+end
+When you call super from inside a method, that tells Ruby to look in the superclass of the current class and find a method with
+the same name as the one from which super is called. If it finds it, Ruby will use the superclass' version of the method.
+Instructions
+1.
+We decided we want to do some chops-punching after all! Let's do two things:
+
+Add puts "Instead of breathing fire..." as the first line in our Dragon's fight method.
+Replace the return statement inside Dragon's definition of fight with the keyword super. (No need to pass any arguments to super,
+since Creature's fight method doesn't take any.)
+=end
+class Creature
+  def initialize(name)
+    @name = name
+  end
+
+  def fight
+    return "Punch to the chops!"
+  end
+end
+
+# Add your code below!
+
+class Dragon < Creature
+  def initialize(name)
+    @name = name
+  end
+
+  def fight
+    return "Breathes fire!"
+  end
+end
+
+
+=begin
+On the flip side, sometimes you'll be working with a derived class (or subclass) and realize that you've overwritten a method or attribute
+defined in that class' base class (also called a parent or superclass) that you actually need. Have no fear! You can directly access the
+attributes or methods of a superclass with Ruby's built-in super keyword.
+
+The syntax looks like this:
+
+class DerivedClass < Base
+  def some_method
+    super(optional args)
+      # Some stuff
+    end
+  end
+end
+When you call super from inside a method, that tells Ruby to look in the superclass of the current class and find a method with the same
+name as the one from which super is called. If it finds it, Ruby will use the superclass' version of the method.
+Instructions
+1.
+We decided we want to do some chops-punching after all! Let's do two things:
+
+Add puts "Instead of breathing fire..." as the first line in our Dragon's fight method.
+Replace the return statement inside Dragon's definition of fight with the keyword super. (No need to pass any arguments to super,
+since Creature's fight method doesn't take any.)
+
+=end
+class Creature
+  def initialize(name)
+    @name = name
+  end
+
+  def fight
+    return "Punch to the chops!"
+  end
+end
+
+# Add your code below!
+
+
+class Dragon < Creature
+  def initialize(name)
+    @name = name
+  end
+
+  def fight
+    puts "Instead of breathing fire..."
+    super()
+  end
+end
+
+=begin
+Any given Ruby class can have only one superclass. Some languages allow a class to have more than one parent, which is a model called multiple inheritance.
+This can get really ugly really fast, which is why Ruby disallows it.
+
+However, there are instances where you want to incorporate data or behavior from several classes into a single class, and Ruby allows this through the use of mixins.
+We'll learn about mixins in a later lesson! For now, we'll demonstrate what happens if you try to do multiple inheritance in Ruby.
+
+The demo code we're about to show you includes a fancy trick: if you want to end a Ruby statement without going to a new line, you can just type a semicolon.
+This means you can write something like
+
+class Monkey
+end
+on just one line: class Monkey; end. This is a time saver when you're writing something very short, like an empty class or method definition.
+Instructions
+Check out the code in the editor. See how we're trying to get Dragon to inherit from Creature and Person? We'll get a superclass mismatch
+for class Dragon error if we try this. Run the code to see for yourself!
+=end
+class Creature
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Dragon < Creature; end
+class Dragon < Person; end
+
+=beginning
+
+Create a class called Message and give it an initialize method. The initialize method should take two parameters,
+from and to, and set them to the instance variables @from and @to (respectively). Don't worry about creating an instance of your class just yet.
+Stuck? Get a hint
+=end
+
+class Message
+  def initialize(from, to)
+  @from = from
+  @to = to
+ end
+end
+
+=begin
+Perfect! Now let's class things up a bit with a class variable.
+Instructions
+1.
+Give your Message class a @@messages_sent class variable and set it equal to 0. In the body of your initialize method,
+increment this value by 1 so that each time a new Message object is created, @@messages_sent will increase by
+1. Don't create any instances of your class just yet!
+=end
+
+class Message
+  @@messages_sent = 0
+  def initialize(from, to)
+  @from = from
+  @to = to
+  @@messages_sent += 1
+ end
+end
+
+=begin
+Perfect! Let's go ahead and create an instance of our Message class.
+Instructions
+1.
+After your class, create a variable called my_message.
+
+Create an instance of your Message class using Message.new with whatever from and to arguments you want! Store the result in my_message.
+=end
+
+class Message
+  @@messages_sent = 0
+  def initialize(from, to)
+  @from = from
+  @to = to
+  @@messages_sent += 1
+ end
+end
+
+my_message = Message.new("Joe", "Tay")
+
+=begin
+Perfect! Now let's get in a little practice with inheritance.
+Instructions
+1.
+Create a second class, Email, that inherits from Message. Give it its own initialize method that takes just one
+parameter, subject, and has one instance variable, @subject, set equal to subject.
+=end
+
+class Email < Message
+  def initialize(subject)
+    @subject = subject
+  end
+end
+
+=begin
+You're a champion! Our last topic: Ruby's super keyword. (We've decided we liked Message's initialize method after all.)
+Instructions
+1.
+To finish this lesson, go ahead and remove the subject parameter and @subject instance variable from Email.
+
+Pass Email's initialize method the same two parameters passed to Message's—from and to—and add the super
+keyword as the only line of code in the body of Email's initialize method.
+=end
+class Message
+  @@messages_sent = 0
+  def initialize(from, to)
+  @from = from
+  @to = to
+  @@messages_sent += 1
+ end
+end
+
+my_message = Message.new("Joe", "Tay")
+
+class Email < Message
+  def initialize(from, to)
+   super
+  end
+end
+
+my_email = Email.new("Michael", "Jacky")
+
+
+# Objeects project!!
+
+class Computer
+  @@users = {}
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password #this makes it so the @@users hash keeps usernames as keys with passwords as the value
+  end
+
+  time = Time.now
+  def create(filename)
+    @files[filename] = time
+    puts "#{filename}, #{@username}"
+  end
+
+  def Computer.get_users
+     @@users
+  end
+end
+
+my_computer = Computer.new("Hal", "123") #this is a username and password!
